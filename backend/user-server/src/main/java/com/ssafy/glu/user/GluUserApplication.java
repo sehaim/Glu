8a4713c9a948b4cb4ce78cf98908c3a304d5807d
이서빈 <bommy00@naver.com>
2024-09-01 16:10:30 +0900

    ✨[Feat] BE user BaseTimeEntity 세팅
    - BaseTimeEntity: 시간 정보를 관리

--- backend/user-server/src/main/java/com/ssafy/glu/user/GluUserApplication.java
+++ backend/user-server/src/main/java/com/ssafy/glu/user/GluUserApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.glu.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GluUserApplication {
 
     public static void main(String[] args) {
