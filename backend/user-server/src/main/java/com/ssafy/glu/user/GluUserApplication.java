9d25f03718574ef03547bacf7fdf513b8a435189
이서빈 <bommy00@naver.com>
2024-09-01 14:51:34 +0900

    🎉[Feat] BE user 프로젝트 초기 세팅

---
+++ backend/user-server/src/main/java/com/ssafy/glu/user/GluUserApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.glu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GluUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GluUserApplication.class, args);
+    }
+
+}
---
+++ backend/user-server/src/test/java/com/ssafy/glu/user/GluUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.glu.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GluUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
