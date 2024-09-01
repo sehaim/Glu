248f4de1d94a7460b269d55db2f855f8afdbe07e
이서빈 <bommy00@naver.com>
2024-09-01 17:38:07 +0900

    🎉[Feat] BE auth 프로젝트 초기 세팅

---
+++ backend/auth-server/src/main/java/com/ssafy/glu/auth/GluAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.glu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GluAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GluAuthApplication.class, args);
+    }
+
+}
