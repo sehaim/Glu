35bf0de32120797f8e4bfbee5ab87c2be510f178
seoyoung927 <seoyoung0000@naver.com>
2024-09-02 17:52:26 +0900

    :tada: [Feat] #S11P21A506-155 #in-progress FE 프로젝트 초기 세팅

---
+++ frontend/glu/src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import GlobalLayout from '@/components/layout/global-layout';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ReactNode } from 'react';
+import { NextPage } from 'next';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Provider } from 'react-redux';
+import { store } from '@/store';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return (
+    <Provider store={store}>
+      <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>
+    </Provider>
+  );
+}
