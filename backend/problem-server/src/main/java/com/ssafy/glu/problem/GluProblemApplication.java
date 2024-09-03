f6e46f745c2d5b8a05d7912fd92230e8151ce026
한수한 <43841214+sem1308@users.noreply.github.com>
2024-09-03 10:21:28 +0900

    ✨[Chore] mongo db 연결 설정

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/GluProblemApplication.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/GluProblemApplication.java
@@ -2,10 +2,10 @@ package com.ssafy.glu.problem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class GluProblemApplication {
 
     public static void main(String[] args) {
