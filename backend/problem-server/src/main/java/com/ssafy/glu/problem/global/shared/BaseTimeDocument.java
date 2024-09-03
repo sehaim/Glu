2f83767a959996229b0ff21972b7a26cc22e170a
한수한 <43841214+sem1308@users.noreply.github.com>
2024-09-03 10:26:28 +0900

    ✨[Feat] BaseTimeEntity mongodb에 맞게 변경

---
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/global/shared/BaseTimeDocument.java
@@ -0,0 +1,19 @@
+package com.ssafy.glu.problem.global.shared;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Document // MongoDB 문서임을 나타냅니다.
+public abstract class BaseTimeDocument {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
