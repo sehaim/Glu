8480a16bd797bce2f517c3bcf5afc42f356aa5ff
한수한 <hshhan0221@naver.com>
2024-09-04 17:50:09 +0900

    ✨[Feat] #S11P21A506-125 #in-progress 질문유형 클래스 수정
    mongodb 관련 어노테이션 추가

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/QuestionType.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/QuestionType.java
@@ -1,15 +1,20 @@
 package com.ssafy.glu.problem.domain.problem.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+@Document
 @Getter
 @AllArgsConstructor
 @Builder
 @ToString
 public class QuestionType {
+	@Id
 	private String questionTypeCode;
 	private String name;
 }
