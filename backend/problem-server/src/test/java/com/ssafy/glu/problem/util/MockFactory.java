8251fb24d8e609cf13f151bd1993696a836aaea8
한수한 <43841214+sem1308@users.noreply.github.com>
2024-09-04 15:54:32 +0900

    ✅[Test] #S11P21A506-125 #in-progress 문제 도메인 클래스 구조 변경

--- backend/problem-server/src/test/java/com/ssafy/glu/problem/util/MockFactory.java
+++ backend/problem-server/src/test/java/com/ssafy/glu/problem/util/MockFactory.java
@@ -1,10 +1,16 @@
 package com.ssafy.glu.problem.util;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
 import com.ssafy.glu.problem.domain.problem.domain.Problem;
 import com.ssafy.glu.problem.domain.problem.domain.ProblemLevel;
+import com.ssafy.glu.problem.domain.problem.domain.ProblemType;
+import com.ssafy.glu.problem.domain.problem.domain.ProblemTypeDetail;
+import com.ssafy.glu.problem.domain.problem.domain.QuestionType;
 
 public class MockFactory {
 
@@ -16,23 +22,67 @@ public class MockFactory {
 		String randomContent = "Content " + UUID.randomUUID().toString().substring(0, 8);
 		String randomSolution = "Solution " + UUID.randomUUID().toString().substring(0, 8);
 
+		Map<String,Object> metadata = new HashMap<>();
+
+		metadata.put("options", List.of("option1", "option2", "option3"));
+		metadata.put("imageUrl", "url");
+
 		return Problem.builder()
 			.title(randomTitle)
 			.content(randomContent)
 			.solution(randomSolution)
 			.level(createProblemLevel())
+			.problemType(createProblemTypeDetail())
+			.questionType(createQuestionType())
+			.metadata(metadata)
 			.build();
 	}
 
 	public static ProblemLevel createProblemLevel() {
 		// 랜덤한 ProblemLevel 객체 생성
-		String randomLevel = "0"+RANDOM.nextInt(3);
-		String randomCode = "PL" + randomLevel;
-		String randomName = "LV " + randomLevel;
+		String level = "0"+RANDOM.nextInt(3);
+		String code = "PL" + level;
+		String name = "LV " + level;
 		return ProblemLevel.builder()
-			.problemLevelCode(randomCode)
-			.name(randomName)
+			.problemLevelCode(code)
+			.name(name)
 			.build();
 	}
+
+	public static ProblemTypeDetail createProblemTypeDetail() {
+		// 랜덤한 ProblemTypeDetail 객체 생성
+		String num = "0"+RANDOM.nextInt(3);
+		String problemTypeDetailCode = "0"+RANDOM.nextInt(3);
+		String name = "유형 " + num;
+		return ProblemTypeDetail.builder()
+			.problemTypeDetailCode(problemTypeDetailCode)
+			.problemType(createProblemType())
+			.name(name)
+			.build();
+	}
+
+	public static ProblemType createProblemType() {
+		// 랜덤한 ProblemTypeDetail 객체 생성
+		String num = "0"+RANDOM.nextInt(3);
+		String problemTypeCode = "PB" + num;
+		String name = "유형 " + num;
+		return ProblemType.builder()
+			.problemTypeCode(problemTypeCode)
+			.name(name)
+			.build();
+	}
+
+	public static QuestionType createQuestionType() {
+		// 랜덤한 ProblemTypeDetail 객체 생성
+		String num = "0"+RANDOM.nextInt(3);
+		String questionTypeCode = "Q" + num;
+		String name = "질문 유형 " + num;
+		return QuestionType.builder()
+			.questionTypeCode(questionTypeCode)
+			.name(name)
+			.build();
+	}
+
+
 
 }
