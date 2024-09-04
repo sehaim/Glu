bbe578159e832310d99ad2241eff690fd417dc5b
한수한 <43841214+sem1308@users.noreply.github.com>
2024-09-04 14:42:00 +0900

    🔧[Chore] 테스트 프로젝트 폴더 변경

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/repository/ProblemRepository.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/repository/ProblemRepository.java
@@ -1,4 +1,4 @@
-package com.ssafy.glu.problem.repository;
+package com.ssafy.glu.problem.domain.problem.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
