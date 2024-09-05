f23517fd3978a16d1b27b8cb8e80d81bb86fc7e6
이서빈 <bommy00@naver.com>
2024-09-05 13:36:39 +0900

    :sparkles:[Feat] #S11P21A506-130 #in-progress UserProblemFavorite 문제 찜 관련 Exception 코드 구현

---
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/exception/ProblemNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ssafy.glu.problem.domain.problem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.ssafy.glu.problem.global.error.ErrorCode;
+import com.ssafy.glu.problem.global.error.ServiceException;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProblemNotFoundException extends ServiceException {
+	public ProblemNotFoundException() {
+		super(ErrorCode.PROBLEM_NOT_FOUND);
+	}
+}
