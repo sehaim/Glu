978c4eeaa227e857aa5b62fb285393a80baeb073
이서빈 <bommy00@naver.com>
2024-09-05 14:38:44 +0900

    ✨[Feat] #S11P21A506-131 #in-progress ProblemService에 문제 찜 취소 관련 메서드 정의

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemService.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemService.java
@@ -15,7 +15,7 @@ public interface ProblemService {
 	// List<ProblemMemoResponse> getProblemMemoList(Long userId, String problemId, Pageable pageable);
 	// List<ProblemBaseResponse> getUserProblemFavoriteList(Long userId, Pageable pageable);
 	void createUserProblemFavorite(Long userId, String problemId);
-	// void deleteUserProblemFavorite(Long userId, String problemId);
+	void deleteUserProblemFavorite(Long userId, String problemId);
 	// TestGradingDetailResponse gradeProblemList(Long userId, ProblemSolveListRequest request);
 	// ProblemGradingResponse gradeProblem(Long userId, String problemId, ProblemSolveRequest request);
 	// TestGradingBaseResponse getPreviousTest(Long userId);
