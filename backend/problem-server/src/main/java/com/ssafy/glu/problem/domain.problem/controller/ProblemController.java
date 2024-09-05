09ae61bd6f6d4586a1c5ec6783b564c8ac8e9212
이서빈 <bommy00@naver.com>
2024-09-05 13:40:14 +0900

    ✨[Feat] #S11P21A506-130 #in-progress ProblemController에 문제 찜 생성 API 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/controller/ProblemController.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/controller/ProblemController.java
@@ -1,5 +1,10 @@
 package com.ssafy.glu.problem.domain.problem.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/problems")
 public class ProblemController {
 	private final ProblemService problemService;
+
+	@PostMapping("/{problemId}/favorite")
+	public ResponseEntity<Void> createUserProblemFavorite(@RequestHeader("userId") Long userId,
+		@PathVariable("problemId") String problemId) {
+		problemService.createUserProblemFavorite(userId, problemId);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 }
--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/UserProblemFavorite.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/UserProblemFavorite.java
@@ -1,25 +1,30 @@
 package com.ssafy.glu.problem.domain.problem.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Getter
 @ToString
 public class UserProblemFavorite {
-    @Id
-    private String userProblemFavoriteId;
+	@Id
+	private String userProblemFavoriteId;
 
-    private final Long userId;
+	private final Long userId;
 
-    private final String problemId;
+	@Field("problemId")
+	@DocumentReference(lazy = true)
+	private final Problem problem;
 
-    @Builder
-    public UserProblemFavorite(Long userId, String problemId) {
-        this.userId = userId;
-        this.problemId = problemId;
-    }
+	@Builder
+	public UserProblemFavorite(Long userId, Problem problem) {
+		this.userId = userId;
+		this.problem = problem;
+	}
 }
