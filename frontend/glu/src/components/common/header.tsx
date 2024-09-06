5a86d3250ff06af41297960a275494e4c3a1ebb3
seoyoung927 <seoyoung0000@naver.com>
2024-09-06 22:02:13 +0900

    :sparkles: [Feat] #S11P21A506-171 #in-progress Navigation bar 구현 - 이벤트 추가

--- frontend/glu/src/components/common/header.tsx
+++ frontend/glu/src/components/common/header.tsx
@@ -1,26 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from './header.module.css';
 
-const getHeaderStyle = (color: string) => {
+function throttle(
+  func: (event: Event) => void,
+  delay: number,
+): (event: Event) => void {
+  let lastCall = 0;
+  return function callback(event: Event) {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) return undefined;
+    lastCall = now;
+    return func(event);
+  };
+}
+
+const getHeaderStyle = (color: string, isScrolled: boolean) => {
   const style: { [key: string]: string } = {};
 
-  if (color === 'white') {
-    style['--background-color'] = 'var(--WHITE)';
-  } else if (color === 'transparent') {
-    style['--background-color'] = 'transparent';
+  if (color === 'transparent') {
+    style['--background-color'] = 'transparent'; // color가 transparent면 항상 투명
+  } else {
+    style['--background-color'] = isScrolled ? 'var(--WHITE)' : 'transparent'; // 스크롤 여부에 따라 색상 변경
   }
 
   return style;
 };
 
 export default function Header({ color }: { color: string }) {
-  const headerStyle = getHeaderStyle(color);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0); // 이전 스크롤 위치를 저장
 
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('Header component rendered with color:', color);
-  }, [color]);
+    const handleScroll = throttle(() => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY >= prevScrollY) {
+        setIsScrolled(true); // 스크롤이 아래로 내려갔을 때 흰색으로 설정
+      } else {
+        setIsScrolled(false); // 스크롤이 위로 올라가면 투명으로 설정
+      }
+
+      setPrevScrollY(currentScrollY); // 이전 스크롤 위치 업데이트
+    }, 500); // 500ms 간격으로 스크롤 이벤트 처리
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollY]);
+
+  const headerStyle = getHeaderStyle(color, isScrolled);
 
   return (
     <div className={styles.container} style={headerStyle}>
