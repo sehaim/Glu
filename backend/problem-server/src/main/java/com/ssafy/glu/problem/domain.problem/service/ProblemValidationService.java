5a078c1ff28c8126761a0e97abb147eb6ac42b82
한수한 <hshhan0221@naver.com>
2024-09-06 14:16:35 +0900

    ✨[Feat] #S11P21A506-125 #done 코드 동작을 위한 interface method 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemValidationService.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemValidationService.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.glu.problem.domain.problem.dto.request.ProblemMemoUpdateRequest;
 import com.ssafy.glu.problem.domain.problem.dto.request.UserProblemLogSearchCondition;
+import com.ssafy.glu.problem.domain.problem.dto.response.ProblemMemoResponse;
 import com.ssafy.glu.problem.domain.problem.dto.response.UserProblemLogResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,21 @@ public class ProblemValidationService implements ProblemService {
 	public Page<UserProblemLogResponse> getProblemListByLog(Long userId, UserProblemLogSearchCondition condition,
 		Pageable pageable) {
 		return problemService.getProblemListByLog(userId, condition, pageable);
+	}
+
+	@Override
+	public ProblemMemoResponse updateProblemMemo(Long userId, String problemMemoId, ProblemMemoUpdateRequest request) {
+		return problemService.updateProblemMemo(userId,problemMemoId,request);
+	}
+
+	@Override
+	public void createUserProblemFavorite(Long userId, String problemId) {
+		problemService.createUserProblemFavorite(userId,problemId);
+	}
+
+	@Override
+	public void deleteUserProblemFavorite(Long userId, String problemId) {
+		problemService.deleteUserProblemFavorite(userId,problemId);
 	}
 }
 
