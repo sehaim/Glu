d7ab3b5010922f7ebd80b97f9f4773768e04bc07
이서빈 <bommy00@naver.com>
2024-09-06 15:13:09 +0900

    ✨[Feat] #S11P21A506-128 #in-progress 메모 삭제 관련 Exception 클래스 및 추가
    - 삭제 관련 Exception 추가

---
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/exception/FavoriteCancelFailedException.java
@@ -0,0 +1,18 @@
+package com.ssafy.glu.problem.domain.problem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.ssafy.glu.problem.global.error.ErrorCode;
+import com.ssafy.glu.problem.global.error.ServiceException;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FavoriteCancelFailedException extends ServiceException {
+	public FavoriteCancelFailedException() {
+		super(ErrorCode.FAVORITE_CANCEL_FAILED);
+	}
+
+	public FavoriteCancelFailedException(Exception exception) {
+		super(ErrorCode.FAVORITE_CANCEL_FAILED, exception);
+	}
+}
---
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/exception/ProblemMemoDeletedUnauthorizedException.java
@@ -0,0 +1,18 @@
+package com.ssafy.glu.problem.domain.problem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.ssafy.glu.problem.global.error.ErrorCode;
+import com.ssafy.glu.problem.global.error.ServiceException;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class ProblemMemoDeletedUnauthorizedException extends ServiceException {
+	public ProblemMemoDeletedUnauthorizedException() {
+		super(ErrorCode.PROBLEM_MEMO_DELETE_UNAUTHORIZED);
+	}
+
+	public ProblemMemoDeletedUnauthorizedException(Exception exception) {
+		super(ErrorCode.PROBLEM_MEMO_DELETE_UNAUTHORIZED, exception);
+	}
+}
