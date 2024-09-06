7ea0e6543c231344aff7fa86379e2ed5bc974be8
이서빈 <bommy00@naver.com>
2024-09-06 15:13:58 +0900

    ✨[Feat] #S11P21A506-128 #in-progress 서비스 인터페이스 및 로직 구현
    - 삭제 관련 메소드 서비스 인터페이스에 추가
    - 삭제 관련 서비스 로직 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemService.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemService.java
@@ -2,22 +2,26 @@ package com.ssafy.glu.problem.domain.problem.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.ssafy.glu.problem.domain.problem.domain.Problem;
+import com.ssafy.glu.problem.domain.problem.domain.ProblemMemo;
 import com.ssafy.glu.problem.domain.problem.dto.request.ProblemMemoUpdateRequest;
 import com.ssafy.glu.problem.domain.problem.dto.request.UserProblemLogSearchCondition;
 import com.ssafy.glu.problem.domain.problem.dto.response.ProblemMemoResponse;
 import com.ssafy.glu.problem.domain.problem.dto.response.UserProblemLogResponse;
 
 public interface ProblemService {
-	List<UserProblemLogResponse> getUserProblemLogList(Long userId, UserProblemLogSearchCondition condition, Pageable pageable);
+	Page<UserProblemLogResponse> getUserProblemLogList(Long userId, UserProblemLogSearchCondition condition, Pageable pageable);
 	// ProblemMemoResponse createProblemMemo(Long userId, String problemId, ProblemMemoCreateRequest request);
 	ProblemMemoResponse updateProblemMemo(Long userId, String problemMemoId, ProblemMemoUpdateRequest request);
-	// void deleteMemo(String problemMemoId);
+	void deleteProblemMemo(Long userId, String problemMemoId);
 	// List<ProblemMemoResponse> getProblemMemoList(Long userId, String problemId, Pageable pageable);
 	// List<ProblemBaseResponse> getUserProblemFavoriteList(Long userId, Pageable pageable);
 	void createUserProblemFavorite(Long userId, String problemId);
 	void deleteUserProblemFavorite(Long userId, String problemId);
+
 	// TestGradingDetailResponse gradeProblemList(Long userId, ProblemSolveListRequest request);
 	// ProblemGradingResponse gradeProblem(Long userId, String problemId, ProblemSolveRequest request);
 	// TestGradingBaseResponse getPreviousTest(Long userId);
