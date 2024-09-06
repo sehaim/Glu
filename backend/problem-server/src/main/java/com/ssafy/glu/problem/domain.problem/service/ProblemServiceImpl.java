86ba9e00ee35f43bbb4d40a6aff7dd1ed6077eaa
이서빈 <bommy00@naver.com>
2024-09-06 10:21:00 +0900

    ✨[Feat] #S11P21A506-127 #in-progress ProblemServiceImpl에 문제 메모 수정 기능 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemServiceImpl.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/service/ProblemServiceImpl.java
@@ -6,12 +6,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.glu.problem.domain.problem.domain.Problem;
+import com.ssafy.glu.problem.domain.problem.domain.ProblemMemo;
 import com.ssafy.glu.problem.domain.problem.domain.UserProblemFavorite;
+import com.ssafy.glu.problem.domain.problem.dto.request.ProblemMemoUpdateRequest;
 import com.ssafy.glu.problem.domain.problem.dto.request.UserProblemLogSearchCondition;
+import com.ssafy.glu.problem.domain.problem.dto.response.ProblemMemoResponse;
 import com.ssafy.glu.problem.domain.problem.dto.response.UserProblemLogResponse;
 import com.ssafy.glu.problem.domain.problem.exception.FavoriteAlreadyRegisteredException;
 import com.ssafy.glu.problem.domain.problem.exception.FavoriteNotFoundException;
+import com.ssafy.glu.problem.domain.problem.exception.ProblemMemoNotFoundException;
+import com.ssafy.glu.problem.domain.problem.exception.ProblemMemoUpdateUnauthorizedException;
 import com.ssafy.glu.problem.domain.problem.exception.ProblemNotFoundException;
+import com.ssafy.glu.problem.domain.problem.repository.ProblemMemoRepository;
 import com.ssafy.glu.problem.domain.problem.repository.ProblemRepository;
 import com.ssafy.glu.problem.domain.problem.repository.UserProblemFavoriteRepository;
 
@@ -23,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ProblemServiceImpl implements ProblemService {
 	private final ProblemRepository problemRepository;
+	private final ProblemMemoRepository problemMemoRepository;
 	private final UserProblemFavoriteRepository userProblemFavoriteRepository;
 
 	@Override
@@ -30,6 +37,21 @@ public class ProblemServiceImpl implements ProblemService {
 		Pageable pageable) {
 
 		return List.of();
+	}
+
+	@Override
+	public ProblemMemoResponse updateProblemMemo(Long userId, String problemMemoId, ProblemMemoUpdateRequest request) {
+		log.info("===== 문제 메모 업데이트 요청 - 메모 Id : {}, 메모 내용 : {} =====", problemMemoId, request);
+
+		// 문제 메모 존재 여부 확인
+		ProblemMemo problemMemo = getProblemMemoOrThrow(problemMemoId);
+
+		// 권한 여부 확인
+		validateProblemMemoUpdateAuthorized(userId, problemMemo);
+
+		problemMemo.updateContents(request.contents());
+
+		return new ProblemMemoResponse(problemMemo.getProblemMemoId(), problemMemo.getContents());
 	}
 
 	@Override
@@ -69,6 +91,19 @@ public class ProblemServiceImpl implements ProblemService {
 	// 문제 존재 여부 판단
 	private Problem getProblemOrThrow(String problemId) {
 		return problemRepository.findById(problemId).orElseThrow(ProblemNotFoundException::new);
+	}
+
+	// 문제 메모 존재 여부 판단
+	private ProblemMemo getProblemMemoOrThrow(String problemMemoId) {
+		return problemMemoRepository.findById(problemMemoId).orElseThrow(ProblemMemoNotFoundException::new);
+	}
+
+	// 사용자가 문제 메모를 수정할 수 있는지 권한 확인
+	private void validateProblemMemoUpdateAuthorized(Long userId, ProblemMemo problemMemo) {
+		if (userId != problemMemo.getUserId()) {
+			log.warn("===== 사용자 [{}]가 문제 메모 [{}]를 수정할 권한이 없습니다 =====", userId, problemMemo);
+			throw new ProblemMemoUpdateUnauthorizedException();
+		}
 	}
 
 	// 이전에 찜 했는지 판단 => 이전에 찜이 있으면 오류
