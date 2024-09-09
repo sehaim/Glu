f3118f769f62049d8a9ec2814c7d4041bdba1045
이서빈 <bommy00@naver.com>
2024-09-09 15:48:03 +0900

    ♻️[Refactor] #S11P21A506-232 #in-progress 불필요한 어노테이션 제거

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/ProblemTypeDetail.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/domain/ProblemTypeDetail.java
@@ -1,20 +1,15 @@
 package com.ssafy.glu.problem.domain.problem.domain;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
-@Document
 @Getter
 @AllArgsConstructor
 @Builder
 @ToString
 public class ProblemTypeDetail {
-	@Id
 	private final String problemTypeDetailCode;
 	private final String name;
 }
