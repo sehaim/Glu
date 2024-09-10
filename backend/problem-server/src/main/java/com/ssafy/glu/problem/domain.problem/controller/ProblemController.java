1ddf8138eef365bef7a698b6ad59e084f49148ca
이서빈 <bommy00@naver.com>
2024-09-10 13:34:51 +0900

    ✨[Feat] #S11P21A506-263 #in-progress ProblemController에 메모 조회 API 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/controller/ProblemController.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain.problem/controller/ProblemController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -73,7 +74,7 @@ public class ProblemController {
 	@GetMapping("/{problemId}/memo")
 	public ResponseEntity<Page<ProblemMemoResponse>> getProblemMemoListInProblem(@RequestHeader(USER_ID) Long userId,
 		@PathVariable("problemId") String problemId,
-		Pageable pageable
+		@PageableDefault(size = 4) Pageable pageable
 	) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(problemService.getProblemMemoList(userId, problemId, pageable));
