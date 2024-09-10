05356a58abd1d9cdb25f262e6b654aa5e7b3deaf
강민제 <kang99086@naver.com>
2024-09-10 14:19:29 +0900

    :recycle:[Refactor] #S11P21A506-195 #done 서버 포트 변경

--- backend/auth-server/src/main/java/com/ssafy/glu/auth/domain/auth/service/JwtTokenService.java
+++ backend/auth-server/src/main/java/com/ssafy/glu/auth/domain/auth/service/JwtTokenService.java
@@ -2,10 +2,10 @@ package com.ssafy.glu.auth.domain.auth.service;
 
 public interface JwtTokenService {
 
-	public void saveRefreshToken(Long userId, String token);
+	void saveRefreshToken(Long userId, String token);
 
-	public String getRefreshToken(Long userId);
+	String getRefreshToken(Long userId);
 
-	public void deleteRefreshToken(Long userId);
+	void deleteRefreshToken(Long userId);
 
 }
