0cc0665d3902f9b49a50d49fac95af0a03baacce
sehaim <seha3722@gmail.com>
2024-09-10 14:34:03 +0900

    :art:[Design] Modal Overlay 수정

--- frontend/glu/src/components/common/modal.tsx
+++ frontend/glu/src/components/common/modal.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { IoClose } from 'react-icons/io5';
 import styles from './modal.module.css';
 import PrimaryButton from './buttons/primaryButton';
@@ -21,9 +23,17 @@ export default function Modal({
     return null;
   }
 
+  const handleOverlayClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div className={styles['modal-overlay']}>
-      <div className={styles.container}>
+    <div className={styles['modal-overlay']} onClick={handleOverlayClick}>
+      <div className={styles.container} onClick={(e) => e.stopPropagation()}>
         <IoClose className={styles['close-icon']} size={30} onClick={onClose} />
         <div className={styles.section}>
           <div className={styles.title}>{title}</div>
