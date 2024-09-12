3749dd95b9dd1ca994df88a72ddac9f495544443
강민제 <kang99086@naver.com>
2024-09-12 10:12:47 +0900

    :recycle:[Refactor] post put에 @requestbody추가

--- backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/controller/UserController.java
+++ backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<Void> register(UserRegisterRequest userRegisterRequest) {
+	public ResponseEntity<Void> register(@RequestBody UserRegisterRequest userRegisterRequest) {
 		userService.register(userRegisterRequest);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
@@ -43,7 +44,7 @@ public class UserController {
 	}
 
 	@PutMapping
-	public ResponseEntity<Void> updateUser(@RequestHeader(USER_ID) Long userId, UserUpdateRequest userUpdateRequest) {
+	public ResponseEntity<Void> updateUser(@RequestHeader(USER_ID) Long userId, @RequestBody UserUpdateRequest userUpdateRequest) {
 		userService.updateUser(userId, userUpdateRequest);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
@@ -67,7 +68,7 @@ public class UserController {
 	}
 
 	@PostMapping("/attendance")
-	public ResponseEntity<Void> attend(@RequestHeader(USER_ID) Long userId, Integer solveNum) {
+	public ResponseEntity<Void> attend(@RequestHeader(USER_ID) Long userId, @RequestBody Integer solveNum) {
 		userService.attend(userId, solveNum);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
