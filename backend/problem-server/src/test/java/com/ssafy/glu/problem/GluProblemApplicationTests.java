415623de3579526ab9aeea03ed1b7d5db2b44e6d
이서빈 <bommy00@naver.com>
2024-09-13 11:19:00 +0900

    🔧[Feat] #S11P21A506-280 #done 불필요한 코드 제거

--- backend/problem-server/src/test/java/com/ssafy/glu/problem/GluProblemApplicationTests.java
+++ backend/problem-server/src/test/java/com/ssafy/glu/problem/GluProblemApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ssafy.glu.problem;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GluProblemApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// package com.ssafy.glu.problem;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class GluProblemApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
