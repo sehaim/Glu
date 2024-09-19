1465690e9abfb4dd3970f6a7b28e3bc4e6af3a83
한수한 <hshhan0221@naver.com>
2024-09-19 14:37:27 +0900

    ✨[Feat] #S11P21A506-209 #in-progress 점수 획득 로직 수정 및 테스트

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/global/util/EloRatingUtil.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/global/util/EloRatingUtil.java
@@ -1,17 +1,17 @@
 package com.ssafy.glu.problem.global.util;
 
 public class EloRatingUtil {
-    private static final int K = 32; // Standard value for K-factor
+    private static final int K = 8; // 최대 얻거나 잃을 수 있는 점수
 
-    private static double calculateExpectedScore(int userScore, int problemScore) {
-        return 1.0 / (1 + Math.pow(10, (problemScore - userScore) / 100.0));
+    private static double calculateExpectedCorrectRate(int userScore, int problemScore) {
+        return 1.0 / (1 + Math.pow(10, (problemScore - userScore) / 400.0));
     }
     // 실제 문제 풀이 결과에 따른 새로운 점수 계산
     public static int calculateNewScore(int userScore, int problemScore, boolean isCorrect) {
-        double expectedScore = calculateExpectedScore(userScore, problemScore);
-        double actualScore = isCorrect ? 1 : 0;  // 맞췄으면 1, 틀렸으면 0
+        double expectedRate = calculateExpectedCorrectRate(userScore, problemScore); // 예상 맞출 확률
+        double actualRate = isCorrect ? 1 : 0;  // 맞췄으면 1, 틀렸으면 0
 
         // 새 점수 = 기존 점수 + K * (실제 점수 - 예상 점수)
-        return (int) (userScore + K * (actualScore - expectedScore));
+        return (int)Math.round(userScore + K * (actualRate - expectedRate));
     }
 }
--- backend/problem-server/src/test/java/com/ssafy/glu/problem/global/util/EloRatingUtilTest.java
+++ backend/problem-server/src/test/java/com/ssafy/glu/problem/global/util/EloRatingUtilTest.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 class EloRatingUtilTest {
 	@Test
-	public void testCalculateNewScore(){
+	public void testCalculateNewScoreWhenCorrect(){
 	    //given
 		int userScore = 350;
 		int problemScore = 400;
@@ -17,5 +20,34 @@ class EloRatingUtilTest {
 
 	    //then
 		assertThat(newScore).isGreaterThan(userScore);
+		log.info("(user : {}, problem : {}, correct : {}) {} -> {}", userScore, problemScore, isCorrect, userScore, newScore);
+	}
+	@Test
+	public void testCalculateNewScoreWhenCorrect2(){
+		//given
+		int userScore = 0;
+		int problemScore = 700;
+		boolean isCorrect = true;
+
+		//when
+		int newScore = EloRatingUtil.calculateNewScore(userScore, problemScore, isCorrect);
+
+		//then
+		assertThat(newScore).isGreaterThan(userScore);
+		log.info("(user : {}, problem : {}, correct : {}) {} -> {}", userScore, problemScore, isCorrect, userScore, newScore);
+	}
+	@Test
+	public void testCalculateNewScoreWhenWrong(){
+		//given
+		int userScore = 350;
+		int problemScore = 400;
+		boolean isCorrect = false;
+
+		//when
+		int newScore = EloRatingUtil.calculateNewScore(userScore, problemScore, isCorrect);
+
+		//then
+		assertThat(newScore).isLessThan(userScore);
+		log.info("(user : {}, problem : {}, correct : {}) {} -> {}", userScore, problemScore, isCorrect, userScore, newScore);
 	}
 }
