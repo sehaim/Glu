bb3e5c10b03e3b9977e7993de2addf2f036563c4
한수한 <hshhan0221@naver.com>
2024-09-20 17:05:57 +0900

    ✨[Feat] #S11P21A506-209 #in-progress 점수 계산 유틸 클래스 생성

---
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/global/util/ScoreUtil.java
@@ -0,0 +1,20 @@
+package com.ssafy.glu.problem.global.util;
+
+import com.ssafy.glu.problem.global.feign.dto.UserProblemTypeResponse;
+
+public class ScoreUtil {
+	public final static int REQUIRED_SCORE = 100;
+
+	// 특정 문제 유형의 총 점수 가져오기
+	public static int calculateTotalScore(int level, int score) {
+		return level * REQUIRED_SCORE + score;
+	}
+
+	public static int calculateTotalScore(UserProblemTypeResponse userProblemType) {
+		return calculateTotalScore(userProblemType.level(), userProblemType.score());
+	}
+
+	public static int calculateNewScore(int currentScore, int problemScore, boolean isCorrect) {
+		return EloRatingUtil.calculateNewScore(currentScore, problemScore, isCorrect);
+	}
+}
