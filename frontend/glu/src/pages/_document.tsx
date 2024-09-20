ab1a1eda9fcd77c91faf00ebbc2a369f38d51f7e
seoyoung927 <seoyoung0000@naver.com>
2024-09-20 16:03:22 +0900

    :bug: [Fix] meta 위치 수정

--- frontend/glu/src/pages/_document.tsx
+++ frontend/glu/src/pages/_document.tsx
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <meta
-        name="google-site-verification"
-        content="V5ZYJCNdt_HzDPX05cwk1u-mp_gc1Oe1jmoD2mFXPko"
-      />
-      <Head />
+      <Head>
+        {/* 메타 태그를 Head 안에 넣어야 합니다 */}
+        <meta
+          name="google-site-verification"
+          content="V5ZYJCNdt_HzDPX05cwk1u-mp_gc1Oe1jmoD2mFXPko"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
