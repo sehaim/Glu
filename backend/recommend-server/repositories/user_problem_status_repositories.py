722749d45e96d7a18d407f487c283a7fb7782308
강민제 <kang99086@naver.com>
2024-09-23 10:55:33 +0900

    :sparkles:[Feat] #S11P21A506-329 내가 푼 문제 제외 조회 API작성

--- backend/recommend-server/repositories/user_problem_status_repositories.py
+++ backend/recommend-server/repositories/user_problem_status_repositories.py
@@ -1,7 +1,10 @@
 from db import mongo_db
 
 # 컬렉션 선택
-collection = mongo_db['userProblemStatus']
+user_problem_status_collection = mongo_db['userProblemStatus']
 
 def get_user_problem_status(user_id):
-    return list(collection.find({'userId': user_id}))
+    return list(user_problem_status_collection.distinct(
+        "problem._id",
+        {"userId": user_id, "status": "CORRECT"}
+    ))
