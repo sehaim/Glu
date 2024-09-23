d580cb770d3c8cb375d1c2d2b80060aff8255f3c
한수한 <hshhan0221@naver.com>
2024-09-23 21:23:54 +0900

    🔨[Fix] #S11P21A506-209 #in-progress 문제 반환시 질문 유형도 반환하도록 수정

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/problem/dto/response/ProblemBaseResponse.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/problem/dto/response/ProblemBaseResponse.java
@@ -1,6 +1,7 @@
 package com.ssafy.glu.problem.domain.problem.dto.response;
 
 import com.ssafy.glu.problem.domain.problem.domain.Problem;
+import com.ssafy.glu.problem.domain.problem.domain.QuestionTypeCode;
 import com.ssafy.glu.problem.domain.problem.domain.UserProblemStatus;
 
 import lombok.Builder;
@@ -13,6 +14,7 @@ public record ProblemBaseResponse(
 	String answer,
 	String solution,
 	Problem.Status status,
+	QuestionTypeCodeResponse questionTypeCode,
 	ProblemLevelResponse problemLevel,
 	ProblemTypeResponse problemType,
 	ProblemTypeDetailResponse problemTypeDetail
@@ -26,10 +28,14 @@ public record ProblemBaseResponse(
 			.content(problem.getContent())
 			.solution(problem.getSolution())
 			.status(status)
+			.questionTypeCode(QuestionTypeCodeResponse.of(problem.getQuestionTypeCode()))
 			.problemLevel(ProblemLevelResponse.of(problem.getProblemLevelCode()))
 			.problemType(ProblemTypeResponse.of(problem.getProblemTypeCode()))
 			.problemTypeDetail(ProblemTypeDetailResponse.of(problem.getProblemTypeDetailCode()))
 			.build();
+	}
+	public static ProblemBaseResponse of(Problem problem) {
+		return of(problem,null);
 	}
 
 	public static ProblemBaseResponse of(UserProblemStatus userProblemStatus) {
