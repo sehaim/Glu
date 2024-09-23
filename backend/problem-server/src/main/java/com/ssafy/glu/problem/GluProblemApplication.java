99f183dabf695ad614fc31c882c479b74c2a48ff
한수한 <hshhan0221@naver.com>
2024-09-23 21:40:33 +0900

    ✨[Feat] #S11P21A506-209 #done Kafka 활성화 및 local로 설정 변경

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/GluProblemApplication.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/GluProblemApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableMongoAuditing
 @EnableFeignClients
+@EnableKafka
 public class GluProblemApplication {
 
     public static void main(String[] args) {
