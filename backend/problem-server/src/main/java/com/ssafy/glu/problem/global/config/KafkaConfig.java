528c573b242d3049ab2b093649373e400dd7ffe3
한수한 <hshhan0221@naver.com>
2024-09-24 17:58:32 +0900

    ✨[Feat] #S11P21A506-209 #done kafka producer 설정 추가

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/global/config/KafkaConfig.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/global/config/KafkaConfig.java
@@ -4,19 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.ssafy.glu.problem.domain.problem.dto.event.ProblemSolvedEvent;
 
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConfig {
 	@Value("${spring.kafka.bootstrap-servers}")
 	String KAFKA_BOOTSTRAP_SERVERS;
 
@@ -25,6 +31,21 @@ public class KafkaConsumerConfig {
 
 	@Value("${kafka.consumer.group-id.user-problem-status}")
 	String GROUP_ID_USER_PROBLEM_STATUS;
+
+
+	@Bean
+	public ProducerFactory<String, ProblemSolvedEvent> producerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
+	public KafkaTemplate<String, ProblemSolvedEvent> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
 
 	@Bean
 	public ConsumerFactory<String, ProblemSolvedEvent> consumerFactory() {
