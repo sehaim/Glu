27cfa197716a87cc02daff99837299bc2ea49c67
이서빈 <bommy00@naver.com>
2024-09-25 18:06:14 +0900

    ♻️[Refactor] #S11P21A506-228 #in-progress Repository 어노테이션 추가

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/test/repository/TestRepository.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/test/repository/TestRepository.java
@@ -1,8 +1,10 @@
 package com.ssafy.glu.problem.domain.test.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.glu.problem.domain.test.domain.Test;
 
+@Repository
 public interface TestRepository extends MongoRepository<Test, String> {
 }
