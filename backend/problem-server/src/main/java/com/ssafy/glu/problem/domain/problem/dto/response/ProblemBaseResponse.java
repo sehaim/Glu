d6e61f6663f6b57a3e4452593912080d6c3b9c48
한수한 <hshhan0221@naver.com>
2024-09-25 14:17:52 +0900

    ✨[Feat] #S11P21A506-318 #in-progress 문제 반환시 metadata, isFavorite 반환하도록 수정

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/problem/dto/response/ProblemBaseResponse.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/problem/dto/response/ProblemBaseResponse.java
@@ -1,5 +1,7 @@
 package com.ssafy.glu.problem.domain.problem.dto.response;
 
+import java.util.Map;
+
 import com.ssafy.glu.problem.domain.problem.domain.Problem;
 import com.ssafy.glu.problem.domain.problem.domain.QuestionTypeCode;
 import com.ssafy.glu.problem.domain.problem.domain.UserProblemStatus;
@@ -17,9 +19,11 @@ public record ProblemBaseResponse(
 	QuestionTypeCodeResponse questionTypeCode,
 	ProblemLevelResponse problemLevel,
 	ProblemTypeResponse problemType,
-	ProblemTypeDetailResponse problemTypeDetail
+	ProblemTypeDetailResponse problemTypeDetail,
+	Map<String,Object> metaData,
+	Boolean isFavorite
 ) {
-	public static ProblemBaseResponse of(Problem problem, Problem.Status status) {
+	public static ProblemBaseResponse of(Problem problem, Problem.Status status, Boolean isFavorite) {
 		if(problem == null) return null;
 		return ProblemBaseResponse.builder()
 			.problemId(problem.getProblemId())
@@ -32,13 +36,18 @@ public record ProblemBaseResponse(
 			.problemLevel(ProblemLevelResponse.of(problem.getProblemLevelCode()))
 			.problemType(ProblemTypeResponse.of(problem.getProblemTypeCode()))
 			.problemTypeDetail(ProblemTypeDetailResponse.of(problem.getProblemTypeDetailCode()))
+			.metaData(problem.getMetadata())
+			.isFavorite(isFavorite)
 			.build();
+	}
+	public static ProblemBaseResponse of(Problem problem, Problem.Status status) {
+		return of(problem, status, null);
 	}
 	public static ProblemBaseResponse of(Problem problem) {
 		return of(problem,null);
 	}
 
 	public static ProblemBaseResponse of(UserProblemStatus userProblemStatus) {
-		return of(userProblemStatus.getProblem(), userProblemStatus.getStatus());
+		return of(userProblemStatus.getProblem(), userProblemStatus.getStatus(), userProblemStatus.getIsFavorite());
 	}
 }
