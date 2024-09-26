c6bbbf9e1c2d3f926d465f18037d47821eb4d652
seoyoung927 <seoyoung0000@naver.com>
2024-09-26 10:05:18 +0900

    :recycle: [Refactor] #S11P21A506-344 #done 종합 테스트 next/image로 변경, nickname 가져오기

--- frontend/glu/src/pages/test/index.tsx
+++ frontend/glu/src/pages/test/index.tsx
@@ -3,15 +3,18 @@ import { useRouter } from 'next/router';
 import { useEffect, useState, useMemo } from 'react';
 import PrimaryButton from '@/components/common/buttons/primaryButton';
 import RadarChart from '@/components/problem/result/radarChart';
-import dummyPreviousTest from '@/mock/dummyPreviousTest.json'; // Import dummy data
+import dummyPreviousTest from '@/mock/dummyPreviousTest.json';
 import { PreviousSolvedProblemType } from '@/types/ProblemTypes';
 import { formatTime, transformProblemType } from '@/utils/problem/result';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store';
 import styles from './test.module.css';
 
 interface ApiResponse {
   testId: number;
   correctCount: number;
-  totalSolveTime: number; // in seconds
+  totalSolveTime: number; // 초 단위
   problemType: PreviousSolvedProblemType[];
 }
 
@@ -23,6 +26,7 @@ export default function Test() {
     PreviousSolvedProblemType[]
   >([]);
   const [, setLoading] = useState<boolean>(true);
+  const username = useSelector((state: RootState) => state.auth.nickname);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -49,17 +53,17 @@ export default function Test() {
   );
 
   const handleButtonClick = () => {
-    router.push('/test/problems'); // Navigate to /test/problems when clicked
+    router.push('/test/problems');
   };
 
   return (
     <div className={styles.container}>
-      {/* Test Recommendation Section */}
-      <div className={styles['content-wrapper']}>
+      <div className={styles['content-wrapper-row']}>
         <div>
           <h2 className={styles['page-title']}>종합 테스트 추천</h2>
           <p>
-            김싸피님을 위한 종합 테스트입니다.
+            <span id={styles.username}>{username}</span>님을 위한 종합
+            테스트입니다.
             <br />
             총 15문제로, 모든 유형이 포함되어 나의 문해력을 종합적으로
             평가합니다.
@@ -67,44 +71,49 @@ export default function Test() {
             지금 바로 시작해보세요!
           </p>
         </div>
-        <PrimaryButton
-          size="large"
-          label="종합테스트 시작"
-          onClick={handleButtonClick}
-        />
+        <div className={styles['button-wrapper']}>
+          <PrimaryButton
+            size="large"
+            label="종합테스트 시작"
+            onClick={handleButtonClick}
+          />
+        </div>
       </div>
 
-      <div className={styles['content-wrapper']}>
-        <div>
-          <h3 className={styles['page-subTitle']}>나의 이전 테스트</h3>
-          <div className={styles['last-test-info']}>
-            <div className={styles['last-test-wrapper']}>
-              <div className={styles['test-info-item-row']}>
-                <p className={styles['info-item-title']}>내 점수</p>
-                <div className={styles['info-item-content']}>
+      <div className={styles['content-wrapper-column']}>
+        <h3 className={styles['page-subTitle']}>나의 이전 테스트</h3>
+        <div className={styles['last-test-wrapper']}>
+          <div className={styles['last-test']}>
+            <div className={styles['last-test-items-column']}>
+              <div className={styles['last-test-item']}>
+                <p className={styles['last-test-item-title']}>내 점수</p>
+                <div className={styles['last-test-item-content']}>
                   {correctCount} / 15
                 </div>
               </div>
-              <div className={styles['test-info-item-row']}>
-                <p className={styles['info-item-title']}>걸린 시간</p>
-                <div className={styles['info-item-content']}>
+              <div className={styles['last-test-item']}>
+                <p className={styles['last-test-item-title']}>걸린 시간</p>
+                <div className={styles['last-test-item-content']}>
                   {formatTime(totalSolveTime)}
                 </div>
               </div>
             </div>
-            <div className={styles['test-info-item']}>
-              <p className={styles['info-item-title']}>영역별 점수</p>
-              <div className={styles['info-item-content']}>
-                <RadarChart problemTypeList={transformedProblemTypeList} />
+            <div className={styles['last-test-items-column']}>
+              <div className={styles['last-test-item-column']}>
+                <p className={styles['last-test-item-title']}>영역별 점수</p>
+                <div className={styles['last-test-item-content-column']}>
+                  <RadarChart problemTypeList={transformedProblemTypeList} />
+                </div>
               </div>
             </div>
           </div>
+          <Image
+            src="/images/glu_character_shadow.png"
+            alt="Glu Character"
+            width={300}
+            height={356}
+          />
         </div>
-        <img
-          className={styles['main-character']}
-          src="/images/glu_character_shadow.png"
-          alt="Glu Character"
-        />
       </div>
     </div>
   );
