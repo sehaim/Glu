d3d0cfebbbf469130781aa2d44acf306e557cedd
이서빈 <bommy00@naver.com>
2024-09-28 12:16:55 +0900

    ✨[Feat] #S11P21A506-231 #in-progress 이전 테스트 조회 관련 Dto 구현

--- backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/test/dto/response/TestGradingBaseResponse.java
+++ backend/problem-server/src/main/java/com/ssafy/glu/problem/domain/test/dto/response/TestGradingBaseResponse.java
@@ -1,4 +1,33 @@
 package com.ssafy.glu.problem.domain.test.dto.response;
 
-public class TestGradingBaseResponse {
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.ssafy.glu.problem.domain.problem.domain.UserProblemLog;
+import com.ssafy.glu.problem.domain.problem.dto.response.TypeGradingResultResponse;
+import com.ssafy.glu.problem.domain.test.domain.Test;
+
+import lombok.Builder;
+
+@Builder
+public record TestGradingBaseResponse(
+	// Test
+	String testId,
+	Integer totalCorrectCount,
+	Integer totalSolvedTime,
+	LocalDateTime createdDate,
+
+	List<TypeGradingResultResponse> gradingResultByTypeList
+) {
+	public static TestGradingBaseResponse of(Test test, List<UserProblemLog> userProblemLogList) {
+		if (test == null)
+			return null;
+		return TestGradingBaseResponse.builder()
+			.testId(test.getTestId())
+			.totalCorrectCount(test.getCorrectCount())
+			.totalSolvedTime(test.getTotalSolveTime())
+			.createdDate(test.getCreatedDate())
+			.gradingResultByTypeList(TypeGradingResultResponse.createGradingResultByTypeList(userProblemLogList))
+			.build();
+	}
 }
