090af35d56396f1b6f9079ce557c3484115fa93a
강민제 <kang99086@naver.com>
2024-10-04 12:42:36 +0900

    :recycle:[Refactor] cors문제 코드 작석

--- backend/recommend-server/main.py
+++ backend/recommend-server/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routers import recommend_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 또는 특정 도메인
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 등록
 app.include_router(recommend_router)
