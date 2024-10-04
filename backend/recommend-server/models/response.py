52cdf8ada48b10385bc2aa04af8c4af12023f643
강민제 <kang99086@naver.com>
2024-10-04 17:28:18 +0900

    :recycle:[Refactor] response형태 변경

--- backend/recommend-server/models/response.py
+++ backend/recommend-server/models/response.py
@@ -9,7 +9,7 @@ from models.model import PydanticObjectId, Problem
 problem_type_details = {
     "PT0111": "옳게 설명하는 문장 고르기",
     "PT0112": "문장의 해당 단어의 유의어/반의어 단어 고르기",
-    "PT0121": "문맥상 올바른 맞춤법 고르기 ",
+    "PT0121": "문맥상 올바른 맞춤법 고르기",
     "PT0211": "명시적 내용 확인하기",
     "PT0221": "뉴스 기사의 핵심어 파악하기",
     "PT0222": "뉴스 기사의 내용 파악하기 - 주장을 뒷받침하는 내용인 것 고르기",
@@ -48,10 +48,8 @@ class QuestionType(BaseModel):
     def create(cls, code: str) -> "QuestionType":
         return cls(
             code=code,
-            name=problem_questions[code]
+            name=problem_questions.get(code, "Unknown")
         )
-
-
 
 class ProblemLevel(BaseModel):
     code: str
@@ -61,7 +59,7 @@ class ProblemLevel(BaseModel):
     def create(cls, code: str) -> "ProblemLevel":
         return cls(
             code=code,
-            name=problem_levels[code]
+            name=problem_levels.get(code, "Unknown")
         )
 
 class ProblemType(BaseModel):
@@ -72,7 +70,7 @@ class ProblemType(BaseModel):
     def create(cls, code: str) -> "ProblemType":
         return cls(
             code=code,
-            name=problem_types[code]
+            name=problem_types.get(code, "Unknown")
         )
 
 class ProblemTypeDetail(BaseModel):
@@ -83,15 +81,14 @@ class ProblemTypeDetail(BaseModel):
     def create(cls, code: str) -> "ProblemTypeDetail":
         return cls(
             code=code,
-            name=problem_type_details[code]
+            name=problem_type_details.get(code, "Unknown")
         )
 
 class Metadata(BaseModel):
     options: List[str]
 
-
 class ProblemResponse(BaseModel):
-    id: PydanticObjectId = Field(alias="_id")  # MongoDB ObjectId와 같은 필드
+    id: PydanticObjectId = Field(alias="_id")
     title: str
     content: str
     answer: int
@@ -112,18 +109,23 @@ class ProblemResponse(BaseModel):
 
     @classmethod
     def from_problem(cls, problem: Problem) -> "ProblemResponse":
-        # ObjectId를 문자열로 변환하여 ProblemResponse 객체 생성
         return cls(
             id=problem.id,
             title=problem.title,
             content=problem.content,
             answer=problem.answer,
             solution=problem.solution,
+            word_count=problem.word_count,
+            word_avg=problem.word_avg,
+            word_hard=problem.word_hard,
+            length=problem.length,
+            classification=problem.classification,
             questionType=QuestionType.create(problem.questionTypeCode),
             problemLevel=ProblemLevel.create(problem.problemLevelCode),
             problemType=ProblemType.create(problem.problemTypeCode),
             problemTypeDetail=ProblemTypeDetail.create(problem.problemTypeDetailCode),
             metadata=problem.metadata,
+            vector=problem.vector,
             createdDate=problem.createdDate,
             modifiedDate=problem.modifiedDate,
         )
