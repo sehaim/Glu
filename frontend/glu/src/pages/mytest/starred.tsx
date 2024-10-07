fadafa3557e81b898eafd4a684788bd0caab644d
seoyoung927 <seoyoung0000@naver.com>
2024-10-07 09:16:53 +0900

    :ambulance: [Fix] 긴급 에러 해결

--- frontend/glu/src/pages/mytest/starred.tsx
+++ frontend/glu/src/pages/mytest/starred.tsx
@@ -1,6 +1,8 @@
-import styles from './mytest.module.css';
+import { GetServerSideProps } from 'next';
 import TestCardList from '@/components/test/testCardList';
+import styles from './mytest.module.css';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {},
