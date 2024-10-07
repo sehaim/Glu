b7105bb64be3fd9b6517020d5ecf2a986a969e0e
sehaim <seha3722@gmail.com>
2024-10-07 16:54:20 +0900

    :sparkles:[Feat] #S11P21A506-351 #in-progress Mytest 해결한 문제 API 연결 구현

--- frontend/glu/src/pages/mytest/correct.tsx
+++ frontend/glu/src/pages/mytest/correct.tsx
@@ -1,9 +1,78 @@
+import { GetServerSideProps } from 'next';
+import { getSolvedTypeTestAPI } from '@/utils/user/mytest';
 import styles from './mytest.module.css';
+import { ProblemType, SolvedProblemResponse } from '@/types/ProblemTypes';
+import MytestTestCardList from '@/components/mytest/mytestTestCardList';
 
-export default function MytestCorrectPage() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const problemData01 = await getSolvedTypeTestAPI(
+    'PT01',
+    0,
+    'CORRECT',
+    undefined,
+    undefined,
+    context,
+  );
+
+  const problemData02 = await getSolvedTypeTestAPI(
+    'PT02',
+    0,
+    'CORRECT',
+    undefined,
+    undefined,
+    context,
+  );
+
+  const problemData03 = await getSolvedTypeTestAPI(
+    'PT03',
+    0,
+    'CORRECT',
+    undefined,
+    undefined,
+    context,
+  );
+
+  const testDataList = [
+    {
+      problemType: { code: 'PT001', name: '어휘 및 문법' },
+      problemData: problemData01,
+    },
+    {
+      problemType: { code: 'PT002', name: '독해' },
+      problemData: problemData02,
+    },
+    {
+      problemType: { code: 'PT003', name: '추론' },
+      problemData: problemData03,
+    },
+  ];
+
+  return {
+    props: {
+      testDataList,
+    },
+  };
+};
+
+interface MytestCorrectPageProps {
+  testDataList: [
+    { problemType: ProblemType; problemData: SolvedProblemResponse },
+  ];
+}
+
+export default function MytestCorrectPage({
+  testDataList,
+}: MytestCorrectPageProps) {
   return (
     <div className={styles.container}>
-      <div>해결한 문제</div>
+      {testDataList.map((testData) => (
+        <MytestTestCardList
+          key={testData.problemType.code}
+          testData={testData?.problemData}
+          problemType={testData?.problemType}
+          pageType="correct"
+        />
+      ))}
     </div>
   );
 }
