2814a8fd25721e8c337989e99a4187ce1b50d95b
강민제 <kang99086@naver.com>
2024-10-08 17:37:18 +0900

    :recycle:[Refactor] 유저 처음 풀때 + (90 * 나이에따른 레벨) 점수 부여

--- backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/domain/UserProblemType.java
+++ backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/domain/UserProblemType.java
@@ -17,6 +17,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 @Entity
 @Getter
 @Builder
@@ -42,13 +45,38 @@ public class UserProblemType extends BaseTimeEntity {
 	@Enumerated(EnumType.STRING)
 	private ProblemTypeCode problemTypeCode;
 
-	public void updateScore(Integer score) {
+	public void updateScore(Integer score, Users user) {
+
+		//처음이면
+		if (user.getIsFirst()) {
+			int age = calculateAge(user.getBirth());
+			int userLevel = calculateUserLevel(age);
+			score += 90 * userLevel;
+		}
+
 		this.score = Math.max(100, score + this.score);
-		this.level = Math.min(Math.max((score / 100), 1), 7);
+		this.level = Math.min(Math.max((this.score / 100), 1), 7);
 	}
 
 	public Integer getScore() {
 		if (score >= 800) return 100;
 		return score % 100;
+	}
+
+	// birthDate는 "yyyy-MM-dd" 형식의 문자열로 입력됩니다.
+	public static int calculateAge(LocalDate birthDate) {
+		// 현재 날짜와의 차이를 계산하여 나이 산출
+		LocalDate today = LocalDate.now();
+		return Period.between(birthDate, today).getYears();
+	}
+
+	public static int calculateUserLevel(int age) {
+		if (age >= 6 && age <= 12) {
+			return age - 5; // 만 6세 -> 1레벨, 만 12세 -> 7레벨
+		} else if (age < 6) {
+			return 1;
+		} else {
+			return 7;
+		}
 	}
 }
--- backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/event/ProblemSolvedEventConsumer.java
+++ backend/user-server/src/main/java/com/ssafy/glu/user/domain/user/event/ProblemSolvedEventConsumer.java
@@ -2,6 +2,9 @@ package com.ssafy.glu.user.domain.user.event;
 
 import java.util.List;
 
+import com.ssafy.glu.user.domain.user.domain.Users;
+import com.ssafy.glu.user.domain.user.exception.UserNotFoundException;
+import com.ssafy.glu.user.domain.user.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ProblemSolvedEventConsumer {
 
 	private final UserProblemTypeRepository userProblemTypeRepository;
+	private final UserRepository userRepository;
+
 
 	@KafkaListener(topics = "${kafka.topic.problem-solved}", groupId = "user-score")
 	@Transactional
@@ -26,10 +31,12 @@ public class ProblemSolvedEventConsumer {
 		log.info("[Kafka] 유저 점수 업데이트, event : {}", event);
 		List<UserProblemType> userProblemTypeList  = userProblemTypeRepository.findAllByUserId(event.userId());
 
+		Users findUser = userRepository.findById(event.userId()).orElseThrow(UserNotFoundException::new);
+
 		for (UserProblemType userProblemType : userProblemTypeList ) {
 			// Enum 비교는 == 사용
 			if (userProblemType.getProblemTypeCode() == event.problem().problemTypeCode()) {
-				userProblemType.updateScore(event.result().acquiredScore());
+				userProblemType.updateScore(event.result().acquiredScore(), findUser);
 				break;
 			}
 		}
