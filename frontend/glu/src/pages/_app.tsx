c2dc678c5d06d04ee15c3c7e91e320d5424f39cd
sehaim <seha3722@gmail.com>
2024-10-14 08:36:46 +0900

    :art:[Design] favicon 수정

--- frontend/glu/src/pages/_app.tsx
+++ frontend/glu/src/pages/_app.tsx
@@ -58,7 +58,7 @@ export default function App({
           content="어린이 문해력, 문해력 진단, 어린이 독서 능력, 문해력 테스트, 어린이 독서 이해력, 글 읽기 능력, 독서 학습, 글루, Glu, 교육 플랫폼, 문해력 향상 프로그램, 학습 능력 평가, 독서 이해력 향상, 초등학생 문해력, 언어 능력 평가"
         />
         <meta name="author" content="ssafy" />
-        <link rel="icon" type="image/png" sizes="32x32" href="logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
       </Head>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
